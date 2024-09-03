9dc42d6a8e3ce7bf2128a380250684d17af865b3
Merja Shourov <merja_shourov@kali.kali_linux>
2024-09-03 16:39:28 +0600

    lading page upload

--- asset/script.js
+++ asset/script.js
@@ -12,10 +12,22 @@ const all_info = [
         "live" : "https://merja-shourov.github.io/javascript-proejcts/palindrome-chacker/",
     },
     {
-        "id": 3,
-        "project_title": "Landing Page",
-        "code" : "https://github.com/merja-shourov/javascript-proejcts/tree/main/palindrome-chacker",
-        "live" : "https://merja-shourov.github.io/javascript-proejcts/digital-clock/",
+        "id": 4,
+        "project_title": "Landing Page( Time Planner )",
+        "code" : "https://github.com/merja-shourov/landing-page-1",
+        "live" : "https://merja-shourov.github.io/landing-page-1/",
+    },
+    {
+        "id": 5,
+        "project_title": "Landing Page (BeGrocery)",    
+        "code" : "https://github.com/merja-shourov/landing-page-2",
+        "live" : "https://merja-shourov.github.io/landing-page-2/",
+    },
+    {
+        "id": 6,
+        "project_title": "Landing Page (Smooth Bekary)",
+        "code" : "https://github.com/merja-shourov/landing-page-3",
+        "live" : "https://merja-shourov.github.io/landing-page-3/",
     }
 ]
 
